9d4c222a8ee4154c88c795e3aa801c702a3c94c9
chetan <zambare.c123@gmail.com>
2020-11-21 15:15:47 +0530

    Add multiple entried in Addressbook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,52 +1,98 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
+	
+	static List<Contact> personList = new ArrayList<Contact>();      // PersonList can be added with Contacts  
+	// addEntry() class will add all entry to addressbook
 	public static void main(String[] args) {
 		System.out.println(" Welcome to Address Book");
 		Scanner sc = new Scanner(System.in);
-		System.out.println(" Please mention details of person in order");
-
-		outer:
 		while(true) {
-			Contact en = new Contact();      // Contact class is created which has details of person which needed to upload, Here just object created
-			System.out.println(" First name");
-			en.setFirst(sc.next());   /// Set first name input taken from console
-			System.out.println(" Last name");
-			en.setLast(sc.next());   /// Set Last name input taken from console
-			System.out.println(" Address");
-			en.setAddress(sc.next());   /// Set Address input taken from console
-			System.out.println(" City");
-			en.setCity(sc.next());   /// Set City input taken from console
-			System.out.println(" State");
-			en.setState(sc.next());   /// Set State input taken from console
-			System.out.println(" Zip");
-			en.setZip(sc.nextInt());   /// Set zip input taken from console
-			System.out.println(" Mobile");
-			en.setMobile(sc.nextLong());   /// Set Mobile input taken from console
-			System.out.println(" Email");
-			en.setEmail(sc.next());   /// Set Email input taken from console
-		
-			// Edit entry step
-			System.out.println(" Do you want to edit existing person detail");
-			System.out.println(" Press 0 to not edit, Press 1 to delete");
+			System.out.println(" What you wish to do");
+			System.out.println(" Press 1 ---Add person \n Press 2 ---Edit existing entry \n Press 3 ---Delete existing entry \n Press 4 ---Display Existing entry \n Press 0 ---Exit ");
 			int num = sc.nextInt();
-			if (num ==0) {   /// If 0 -- Edit is not required
-				break;
+			if (num == 0) { /// If 0 option from exit 
+				break;   
 			}
-			else if (num == 1) {   /// If 1 -- Edit is required check for first name
-				while(true) {
-					System.out.println(" Mention First name of person whose entry want to Delete");
-					String personName = sc.next();
-					if (en.getFirst().equals(personName)) {
-						en = null ;    /// assigning object to null will delete person entry
-					    break outer;
-					}
-					else {
-						System.out.println(" Name not found in addressbook ");
-					}
+			else if(num == 1) {  /// If 1 is person details are  intended to add
+				System.out.println(" Please mention details of person in order");
+				personList.add(addEntry());
+			}
+			else if(num == 2) {  /// If 2 is person details are  intended to edit
+				System.out.println(" Please mention first name of person");
+				String name = sc.next();
+				if (findEntry(name)) { // When we invoke finEntry() we delete that object from list with first name
+					System.out.println(" Please edit details of person in order");
+					personList.add(addEntry());    // addEntry call agains and again entry got added
+				}
+				else {
+					System.out.println(" Can not find the given entry");
 				}
 			}
+			else if (num == 3){  /// If 3 is person details are  intended to delete
+				System.out.println(" Please mention first name of person");
+				String name = sc.next();
+				if (findEntry(name)) {
+					System.out.println(" Entry of person ");
+				}
+				else {
+					System.out.println(" Can not find the given entry");
+				}
+			}
+			else if(num ==4) {
+				display();
+			}
+			else {
+				System.out.println("Invalid entry");
+			}
+			
 		}
+		
+	}
+	
+	public static Contact addEntry() {  // addEntry will add all entry to class
+		Contact en = new Contact();      // Contact class is created which has details of person which needed to upload, Here just object created
+		System.out.println(" First name");
+		Scanner sc = new Scanner(System.in);
+		en.setFirst(sc.next());   /// Set first name input taken from console
+		System.out.println(" Last name");
+		en.setLast(sc.next());   /// Set Last name input taken from console
+		System.out.println(" Address");
+		en.setAddress(sc.next());   /// Set Address input taken from console
+		System.out.println(" City");
+		en.setCity(sc.next());   /// Set City input taken from console
+		System.out.println(" State");
+		en.setState(sc.next());   /// Set State input taken from console
+		System.out.println(" Zip");
+		en.setZip(sc.nextInt());   /// Set zip input taken from console
+		System.out.println(" Mobile");
+		en.setMobile(sc.nextLong());   /// Set Mobile input taken from console
+		System.out.println(" Email");
+		en.setEmail(sc.next());   /// Set Email input taken from console
+		return en;
+	}
+	
+	public static boolean findEntry(String name) {
+		for(Object Obj :personList) {
+			String FirstName = ((Contact) Obj).getFirst();
+			if(FirstName.equals(name)) {
+				personList.remove(Obj);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static void display() {
+		for(Object Obj :personList) {
+			System.out.println(Obj);
+		}
+	}
+	public String toString() {
+		//display();
+		return   "Addressbook ";
 	}
 
 }
