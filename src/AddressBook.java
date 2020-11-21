2629ab72cf3b80b28ebac4fb7092b380605f3901
chetan <zambare.c123@gmail.com>
2020-11-21 14:09:41 +0530

    Contact lass is created and In AddressBook Contact object created

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -6,6 +6,7 @@ public class AddressBook {
 		Scanner sc = new Scanner(System.in);
 		System.out.println(" Please mention details of person in order");
 		Contact en = new Contact();      // Contact class is created which has details of person which needed to upload, Here just object created
+		sc.close();
 	}
 
 }
