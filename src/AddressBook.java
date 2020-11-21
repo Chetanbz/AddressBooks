dbc48087e2639c82631c52080f1386b24caa5d96
chetan <zambare.c123@gmail.com>
2020-11-21 14:08:17 +0530

    Contact lass is created and In AddressBook Contact object created

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 
 public class AddressBook {
 	public static void main(String[] args) {
 		System.out.println(" Welcome to Address Book");
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Please mention details of person in order");
+		Contact en = new Contact();      // Contact class is created which has details of person which needed to upload, Here just object created
 	}
 
 }
---
+++ src/Contact.java
@@ -0,0 +1,69 @@
+
+public class Contact {
+	private String first;
+	private String last;
+	private String address;
+	private String city;
+	private String State;
+	private int zip;
+	private int mobile;
+	private String email;
+	
+	
+	
+	public Contact() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getFirst() {
+		return first;
+	}
+	public void setFirst(String first) {
+		this.first = first;
+	}
+	public String getLast() {
+		return last;
+	}
+	public void setLast(String last) {
+		this.last = last;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	public int getMobile() {
+		return mobile;
+	}
+	public void setMobile(int mobile) {
+		this.mobile = mobile;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String toString() {
+		return  "#" + first + " " + last + " " + address + " " + city + " " + State + " " + zip+ " " + mobile + " " + email; 
+	}
+
+}
